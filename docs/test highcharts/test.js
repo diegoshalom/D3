f88c6a0a66eb72f60605bd445df960849553c5c6
diegoshalom <diegoshalom@gmail.com>
2019-12-20 23:45:21 -0300

    test

--- docs/test highcharts/test.js
+++ docs/test highcharts/test.js
@@ -9,15 +9,30 @@ function updateGraph(){
             title: {
               text: 'Live Data (CSV)'
             },
-          
+            xAxis: {
+                type: 'datetime',
+            },
             subtitle: {
               text: 'Data input from a remote CSV file'
             },
           
             data: {
-              csvURL: 'https://demo-live-data.highcharts.com/time-data.csv',
-              enablePolling: false
+              csvURL: 'https://dl.dropbox.com/s/844z5wp7nx2dkie/tempe_mientras.csv?dl=0',
+              //csvURL: 'https://dl.dropbox.com/s/tvuk81w7z773ax1/test.csv?dl=0',
+              enablePolling: true,
+              dateFormat: "dd-mmm-yyyy-hh:mm",
+              //parseDate: function(date, d) {
+              //  return Date.parse(date);
+              //},              
+              complete: function(o) {
+                o.series = [
+                  o.series[6],
+                  o.series[7],
+                  o.series[8],
+                ]
+              }
             }
+
           });
           
 

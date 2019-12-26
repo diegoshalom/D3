6493e1e0b605fa00926f4284e5e7da3a8053922b
diegoshalom <diegoshalom@gmail.com>
2019-12-26 11:15:05 -0300

    cambio url del csv en dropbox

--- docs/test highcharts/test.js
+++ docs/test highcharts/test.js
@@ -25,7 +25,8 @@ function updateGraph(){
             },
           
             data: {
-              csvURL: 'https://dl.dropbox.com/s/844z5wp7nx2dkie/tempe_mientras.csv?dl=0',
+              csvURL: 'https://dl.dropbox.com/s/x1s4i8gtznll2dj/tempe_mientras.csv?dl=0', 
+              //csvURL: 'https://dl.dropbox.com/s/844z5wp7nx2dkie/tempe_mientras.csv?dl=0',
               //csvURL: 'https://dl.dropbox.com/s/tvuk81w7z773ax1/test.csv?dl=0',
               enablePolling: true,
               dateFormat: "dd-mmm-yyyy-hh:mm",
